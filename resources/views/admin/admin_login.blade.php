e1a4c07ca83d146d4fc244198253810333787b6b
aniszariat <zariatanis.az@gmail.com>
2024-01-01 22:10:49 +0100

    admin assets path & admin template refreach

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -34,22 +34,22 @@ License: For each use you must have a valid license purchased only from above li
     <!-- End fonts -->
 
     <!-- core:css -->
-    <link rel="stylesheet" href="{{asset('../../../assets/vendors/core/core.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/vendors/core/core.css')}}">
     <!-- endinject -->
 
     <!-- Plugin css for this page -->
     <!-- End plugin css for this page -->
 
     <!-- inject:css -->
-    <link rel="stylesheet" href="{{asset('../../../assets/fonts/feather-font/css/iconfont.css')}}">
-    <link rel="stylesheet" href="{{asset('../../../assets/vendors/flag-icon-css/css/flag-icon.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/fonts/feather-font/css/iconfont.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/vendors/flag-icon-css/css/flag-icon.min.css')}}">
     <!-- endinject -->
 
     <!-- Layout styles -->
-    <link rel="stylesheet" href="{{asset('../../../assets/css/demo2/style.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/css/demo2/style.css')}}">
     <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="{{asset('../../../assets/images/favicon.png')}}" />
+    <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.png')}}" />
 </head>
 
 <body>
@@ -71,6 +71,7 @@ License: For each use you must have a valid license purchased only from above li
                                         <a href="#" class="noble-ui-logo logo-light d-block mb-2">Noble<span>UI</span></a>
                                         <h5 class="text-muted fw-normal mb-4">Welcome back! Log in to your account.</h5>
                                         <form class="forms-sample" method="POST" action="{{ route('login') }}">
+                                            <!-- <form class="forms-sample" method="POST" action="{{ route('admin.dashboard') }}"> -->
                                             @csrf
                                             <div class="mb-3">
                                                 <label for="login" class="form-label">Email/Name/Phone</label>
@@ -105,15 +106,15 @@ License: For each use you must have a valid license purchased only from above li
     </div>
 
     <!-- core:js -->
-    <script src="{{asset('../../../assets/vendors/core/core.js')}}"></script>
+    <script src="{{asset('backend/assets/vendors/core/core.js')}}"></script>
     <!-- endinject -->
 
     <!-- Plugin js for this page -->
     <!-- End plugin js for this page -->
 
     <!-- inject:js -->
-    <script src="{{asset('../../../assets/vendors/feather-icons/feather.min.js')}}"></script>
-    <script src="{{asset('../../../assets/js/template.js')}}"></script>
+    <script src="{{asset('backend/assets/vendors/feather-icons/feather.min.js')}}"></script>
+    <script src="{{asset('backend/assets/js/template.js')}}"></script>
     <!-- endinject -->
 
     <!-- Custom js for this page -->
