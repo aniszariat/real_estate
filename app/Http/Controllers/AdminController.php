7465eeb6ae64d3f2894c9790a8fe0bfe0df33f0c
aniszariat <zariatanis.az@gmail.com>
2024-01-02 13:42:01 +0100

    Adding Toster In For View Message

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -51,11 +51,16 @@ class AdminController extends Controller
         $data->phone = $request->phone;
         if ($request->file('photo')) {
             $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/' . $data->photo));
             $filename = date('YmdHi') . $file->getClientOriginalName();
             $file->move(public_path('upload/admin_images'), $filename);
             $data['photo'] = $filename;
         }
         $data->save();
-        return redirect()->back();
+        $notification = array(
+            'message' => 'Admin profile updated successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
     }
 }
