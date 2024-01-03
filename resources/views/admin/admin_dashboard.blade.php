7ab943f0cb9df8d68151fb50efc1df3fbdc7918d
aniszariat <zariatanis.az@gmail.com>
2024-01-03 08:59:05 +0100

    Property Type Crud

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -17,6 +17,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     <!-- End fonts -->
 
+
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="{{asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css')}}">
+    <!-- End plugin css for this page -->
+
     <!-- core:css -->
     <link rel="stylesheet" href="{{asset('backend/assets/vendors/core/core.css')}}">
     <!-- endinject -->
@@ -36,7 +41,6 @@
 
     <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.png')}}" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
-
 </head>
 
 <body>
@@ -81,6 +85,12 @@
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
+
+    <!-- DATA table -->
+    <script src="{{asset('backend/assets/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js')}}"></script>
+    <script src="{{asset('backend/assets/js/data-table.js')}}"></script>
+    <!-- End DATA table -->
     <script>
         @if(Session::has('message'))
         var type = "{{ Session::get('alert-type','info') }}"
@@ -103,6 +113,7 @@
         }
         @endif
     </script>
+
 </body>
 
 </html>
